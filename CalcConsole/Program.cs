ea2c0e39c660533688567517973ed13c7b28d126
quesyx <Emplolx@gmail.com>
2022-01-25 20:48:09 +0500

    Create Calc with UnitTests

---
+++ CalcConsole/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using CalcLib;
+namespace CalcConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    double firstNum, secondNum = 0;
+                    string action;
+                    Console.Write("Введите 1 число ");
+                    firstNum = double.Parse(Console.ReadLine());
+                    if ((firstNum > 99999999999) || (firstNum == 0))
+                    {
+                        Console.WriteLine("Нельзя вводить число больше 999999999 и 0");
+                        Console.ReadLine();
+                        continue;
+                    }
+                    Console.Write("Введите действие (+, -, *, /, s(Корен)) ");
+                    action = Console.ReadLine();
+                    Console.WriteLine();
+                    if (action != "s")
+                    {
+                        Console.Write("Введите 2 число ");
+                        secondNum = double.Parse(Console.ReadLine());
+                    }
+                    var calc = new CalcActions();
+                    switch (action)
+                    {
+                        case "+":
+                            Console.WriteLine("{0}+{1}={2}", firstNum, secondNum, calc.Sum(firstNum, secondNum));
+                            break;
+                        case "-":
+                            Console.WriteLine("{0}-{1}={2}", firstNum, secondNum, calc.Diff(firstNum, secondNum));
+                            break;
+                        case "*":
+                            Console.WriteLine("{0}*{1}={2}", firstNum, secondNum, calc.Mult(firstNum, secondNum));
+                            break;
+                        case "/":
+                            try
+                            {
+                                if (secondNum == 0)
+                                    throw new DivideByZeroException();
+                                Console.WriteLine("{0}/{1}={2}", firstNum, secondNum, calc.Div(firstNum, secondNum));
+                            }
+                            catch (DivideByZeroException)
+                            {
+                                Console.WriteLine("Делить на ноль нельзя");
+
+                            }
+                            break;
+                        case "s":
+                            Console.WriteLine("Корен({0})={1}", firstNum, calc.Koren(firstNum));
+                            break;
+                        default:
+                            Console.WriteLine("Ошибка, Некорректное действие!");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ CalcLib/CalcActions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalcLib
+{
+    public  class CalcActions
+    {
+        public  double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public  double Diff(double a, double b)
+        {
+            return a - b;
+        }
+        public  double Mult(double a, double b)
+        {
+            return a * b;
+        }
+        public  double Div(double a, double b)
+        {
+            return a / b;
+        }
+        public  double Koren(double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
